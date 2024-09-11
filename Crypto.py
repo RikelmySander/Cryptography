12ea41703be8d2b116ef03285bf33b51fcafeedf
RikelmySander <rikelmysander@gmail.com>
2024-09-11 01:09:33 -0300

    First Version - Alpha 1.0

---
+++ Crypto.py
@@ -0,0 +1,24 @@
+from random import randint
+
+binario = ""
+Cryptografia = input("enter a phrase to be encrypted:")
+
+#for bit in range(Cryptografia):
+#    binario += str(randint(0 , 1))
+#
+#inteiro = int(binario)
+#print(bin(inteiro))
+#print(binario.bit_length(binario))
+# for x in Cryptografia.encode("utf-8"):     
+#     print(x, bin(x), sep="\t")
+      
+caracteres = dict()
+
+for bit in Cryptografia:
+    # print("Caractere: {}".format(bit))
+    # print(f"ascii: {ord(bit)}")
+    # print("Binário: {}".format(bin(int(ord(bit)))))
+    caracteres[bit] = bin(int(ord(bit)))
+    print("-="*30)
+
+print(caracteres)
